2e7026ebbe635103640f2cc55aa00cdb400b3d7e
Dre <daanimasaun@gmail.com>
2025-08-08 13:54:23 +0100

    week 10

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,6 +4,7 @@ from django.views.generic.detail import DetailView
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from django.contrib.auth import login
 
 
 # Function-based view
@@ -20,4 +21,4 @@ class LibraryDetailView(DetailView):
 class Register(CreateView):
     form_class = UserCreationForm
     template_name = 'relationship_app/register.html'
-    success_url = reverse_lazy('login.html')
+    success_url = reverse_lazy('login')
