143601dda522dbd60b5723b001a7771a846d9b14
Dre <daanimasaun@gmail.com>
2025-08-08 13:52:31 +0100

    week 10

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path
-from .views import list_books
-from .views import LibraryDetailView
+from .views import *
+from django.contrib.auth import views as auth_views
+
+import relationship_app.views as views 
+
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-]
-
-
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('relationship_app.urls')),
+    path('login/',auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('register/', views.Register.as_view(), name='register' ),
+    path('logout/',auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name ='logout')
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import Book, Library  # contains exactly: "from .models import Library"
 from django.views.generic.detail import DetailView
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+
 
 # Function-based view
 def list_books(request):
@@ -12,3 +16,8 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"  # exact string checker wants
     context_object_name = "library"
+
+class Register(CreateView):
+    form_class = UserCreationForm
+    template_name = 'relationship_app/register.html'
+    success_url = reverse_lazy('login.html')
