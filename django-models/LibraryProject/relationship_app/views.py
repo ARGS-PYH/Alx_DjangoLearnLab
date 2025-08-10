d3b17b130faeaa1731950f032568b1a890ff3d47
Dre <daanimasaun@gmail.com>
2025-08-10 19:30:58 +0100

    contination of week 10

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,7 +4,7 @@ from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.contrib.auth import login
-from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import user_passes_test, permission_required  # Added permission_required
 
 from .models import Book, Library
 
@@ -26,7 +26,6 @@ class Register(CreateView):
     template_name = 'relationship_app/register.html'
     success_url = reverse_lazy('login')
 
-    
     def form_valid(self, form):
         response = super().form_valid(form)
         user = form.save()
@@ -34,15 +33,32 @@ class Register(CreateView):
         return redirect('list_books')
 
 
-
 @user_passes_test(lambda u: u.is_authenticated and u.userprofile.role == 'Admin')
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
+
 
 @user_passes_test(lambda u: u.is_authenticated and u.userprofile.role == 'Librarian')
 def librarian_view(request):
     return render(request, 'relationship_app/librarian_view.html')
 
+
 @user_passes_test(lambda u: u.is_authenticated and u.userprofile.role == 'Member')
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
+
+
+
+@permission_required('relationship_app.can_add_book')
+def add_book_view(request):
+    return render(request, 'relationship_app/add_book.html')
+
+
+@permission_required('relationship_app.can_change_book')
+def change_book_view(request):
+    return render(request, 'relationship_app/change_book.html')
+
+
+@permission_required('relationship_app.can_delete_book')
+def delete_book_view(request):
+    return render(request, 'relationship_app/delete_book.html')
