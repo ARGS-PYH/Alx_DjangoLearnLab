23645b2b5a3cdace979b6d6de9ea7186e009ee73
Dre <daanimasaun@gmail.com>
2025-08-19 14:43:39 +0100

    week11 assignment -01

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,15 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
+
+
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     publication_year = models.IntegerField()
 
+    class Meta:
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+        ]
+
     def __str__(self):
         return f"{self.title} by {self.author} ({self.publication_year})"
-
 
 
 class CustomUserManager(BaseUserManager):
