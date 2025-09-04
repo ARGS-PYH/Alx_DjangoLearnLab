72d8a71d2391dd20d2bf1e3ae9d34e04d1e096ec
Dre <daanimasaun@gmail.com>
2025-09-04 22:05:40 +0100

    Week 13 assignment 3

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework as django_filters
 from .models import Book
 from .serializers import BookSerializer
 
