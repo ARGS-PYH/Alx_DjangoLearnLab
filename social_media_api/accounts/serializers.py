2f5e6d5b7d5328b0bb3673874f490ad9208a3855
Dre <daanimasaun@gmail.com>
2025-09-11 10:42:16 +0100

    Week 15 correction

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,33 +1,31 @@
-from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from rest_framework.authtoken.models import Token
+from rest_framework import serializers
 
 User = get_user_model()
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ["id", "username", "email", "bio", "profile_picture", "followers"]
-        read_only_fields = ["followers", "id"]
-
-class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, min_length=8)
-    password2 = serializers.CharField(write_only=True, label="Confirm password")
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)   
+    password2 = serializers.CharField(write_only=True) 
 
     class Meta:
         model = User
-        fields = ["username", "email", "password", "password2", "bio", "profile_picture"]
+        fields = ['id', 'username', 'email', 'password', 'password2', 'bio', 'profile_picture']
 
     def validate(self, data):
-        if data["password"] != data["password2"]:
-            raise serializers.ValidationError({"password": "Passwords must match."})
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError("Passwords do not match.")
         return data
 
     def create(self, validated_data):
-        validated_data.pop("password2", None)
-        password = validated_data.pop("password")
-        user = User(**validated_data)
-        user.set_password(password)
-        user.save()
-        Token.objects.create(user=user)
+        validated_data.pop('password2')
+        password = validated_data.pop('password')
+        user = User.objects.create_user(   
+            **validated_data,
+            password=password
+        )
         return user
+
+
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
