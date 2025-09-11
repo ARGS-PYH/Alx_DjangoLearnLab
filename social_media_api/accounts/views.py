faa443bd08f5b8d1c76e2e14d6d9943a8c53d6da
Dre <daanimasaun@gmail.com>
2025-09-11 15:18:35 +0100

    Week 15 correction

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,39 +1,45 @@
 from django.contrib.auth import get_user_model
-from rest_framework import serializers
+from rest_framework import generics, permissions
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
+from .serializers import RegisterSerializer, UserSerializer, UserLoginSerializer
 
 User = get_user_model()
 
-class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-    password2 = serializers.CharField(write_only=True)
 
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'password', 'password2', 'bio', 'profile_picture']
+class RegisterAPIView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
 
-    def validate(self, data):
-        if data['password'] != data['password2']:
-            raise serializers.ValidationError("Passwords do not match.")
-        return data
-
-    def create(self, validated_data):
-        validated_data.pop('password2')
-        password = validated_data.pop('password')
-        user = get_user_model().objects.create_user(
-            **validated_data,
-            password=password
-        )
-        Token.objects.create(user=user)
-        return user
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        user = User.objects.get(id=response.data['id'])
+        token, created = Token.objects.get_or_create(user=user)
+        response.data['token'] = token.key
+        return response
 
 
-class UserLoginSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField(write_only=True)
+class CustomObtainAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.id,
+            'username': user.username
+        })
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture']
+class ProfileAPIView(generics.RetrieveUpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
