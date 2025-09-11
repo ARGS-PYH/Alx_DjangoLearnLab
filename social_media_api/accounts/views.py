907803b2dd0428a85319b0339efafa04327adfa6
Dre <daanimasaun@gmail.com>
2025-09-11 15:27:59 +0100

    Week 15 correction

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,33 +1,33 @@
-from django.contrib.auth import get_user_model
-from rest_framework import serializers
+from rest_framework import generics, permissions
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+from .serializers import RegisterSerializer, UserSerializer, UserLoginSerializer
 
 User = get_user_model()
 
-class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)   
-    password2 = serializers.CharField(write_only=True) 
-
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'password', 'password2', 'bio', 'profile_picture']
-
-    def validate(self, data):
-        if data['password'] != data['password2']:
-            raise serializers.ValidationError("Passwords do not match.")
-        return data
-
-    def create(self, validated_data):
-        validated_data.pop('password2')
-        password = validated_data.pop('password')
-        user = get_user_model().objects.create_user(
-            **validated_data,
-            password=password
-        )
-        Token.objects.create(user=user)
-        return user
+class RegisterAPIView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
 
 
-class UserLoginSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField(write_only=True)
+class ProfileAPIView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
+
+class CustomObtainAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        token = Token.objects.get(key=response.data['token'])
+        return Response({
+            'token': token.key,
+            'user_id': token.user_id,
+            'username': token.user.username
+        })
